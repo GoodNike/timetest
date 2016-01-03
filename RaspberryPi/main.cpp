a2bd07291f3e162c52ad35f1789dcfbe2ca1f17e
Nikolay Krasheninnikov <nkrasheninnikov@yandex.ru>
2016-01-03 18:23:06 +0300

    Для повышения гибкости управление программой осуществляется через командную строку: задается номер пина для выдачи сигнала и дискретность работы.

--- RaspberryPi/main.cpp
+++ RaspberryPi/main.cpp
@@ -1,3 +1,17 @@
+/**
+ * @copyright   Николай Крашенинников
+ * @project     Bottom project
+ * @file        main.cpp
+ * @brief       Программа для оценки точности выдерживания RaspberryPi собственной дискретности.
+ *
+ * Программа циклично меняет состояние пина GPIO с HIGH на LOW и обратно. Пользователь задает номер
+ * пина для выдачи, задает дискретность работы, мс, через командную строку. В случае успешного
+ * запуска, программа не возвращает управление.
+ */
+
+#include <iostream>
+#include <cstdlib>
+
 #include <string.h>
 #include <sched.h>
 #include <sys/mman.h>
@@ -6,31 +20,39 @@
 
 int main(int argc, char **argv)
 {
-    const uint8_t PIN = 4;
-
-    if (!bcm2835_init()) {
+    if (argc != 3) {
+        std::cout << "Usage: timetest <pin_number> <disc>" << std::endl;
         return 1;   // error
     }
 
-    bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+    uint8_t pin = std::atoi(argv[1]);
+    std::cout << "Using pin: " << (uint32_t) pin << std::endl;
+    unsigned int ms = std::atoi(argv[2]);
+    std::cout << "Using disc: " << (uint32_t) ms << std::endl;
 
-    uint8_t pin_level = HIGH;
+    if (!bcm2835_init()) {
+        std::cout << "bcm2835_init() failure" << std::endl;
+        return 1;   // error
+    }
 
-    struct sched_param sp;
+    sched_param sp;
     memset(&sp, 0, sizeof(sp));
     sp.sched_priority = sched_get_priority_max(SCHED_FIFO);
     sched_setscheduler(0, SCHED_FIFO, &sp);
     mlockall(MCL_CURRENT | MCL_FUTURE);
 
+    bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP);
+    uint8_t pin_level = HIGH;
+
     while (true)
     {
-        bcm2835_gpio_write(PIN, pin_level);
+        bcm2835_gpio_write(pin, pin_level);
         if (pin_level == HIGH) {
             pin_level = LOW;
         } else {
             pin_level = HIGH;
         }
-        bcm2835_delay(8);
+        bcm2835_delay(ms);
     }
 
     bcm2835_close();
